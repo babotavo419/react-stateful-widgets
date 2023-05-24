5828207bdbe850782f2143d09bc71064b3fba464
babotavo419 <one.faith419@gmail.com>
2023-05-24 15:01:39 -0400

    fourth com done

--- src/components/Input.js
+++ src/components/Input.js
@@ -40,10 +40,7 @@ export default function Input() {
   /* STEP 1 */
 
   const changeInput = evt => {
-    // When the input changes, its whole value can be found inside the event object.
-    // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-
     /* STEP 4 */
   };
   const reset = () => {
